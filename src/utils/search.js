34e2d3d7f8951638784c26ddceee46ae7dec849a
amira-mohiey <amira.mohiey@cartologic.com>
2018-07-19 20:23:15 +0200

    modify tests

--- src/utils/search.js
+++ src/utils/search.js
@@ -24,7 +24,6 @@ export function searchByName(text, availableHotels) {
 }
 export function searchByPrice(maxPrice, availableHotels, nights) {
   var price = Math.round(maxPrice / nights);
-
   var hotels = availableHotels.hotels.filter(hotel => {
     return Math.round(hotel.price) <= price;
   });
@@ -35,10 +34,11 @@ export function searchByPrice(maxPrice, availableHotels, nights) {
 export function search(filters, allHotels) {
   var hotels = {};
   if (filters.name && filters.price) {
-    var nameHotelsArr=searchByName(filters.name, allHotels).hotels
-    var priceHotelsArr= searchByPrice(filters.price, allHotels, filters.nights).hotels
-    hotels = nameHotelsArr.filter(value =>
-         -1 !==priceHotelsArr.indexOf(value)
+    var nameHotelsArr = searchByName(filters.name, allHotels).hotels;
+    var priceHotelsArr = searchByPrice(filters.price, allHotels, filters.nights)
+      .hotels;
+    hotels = nameHotelsArr.filter(
+      value => -1 !== priceHotelsArr.indexOf(value)
     );
     return { hotels };
   } else {
@@ -46,7 +46,6 @@ export function search(filters, allHotels) {
       hotels = searchByName(filters.name, allHotels);
     }
     if (filters.price) {
-  
       hotels = searchByPrice(filters.price, allHotels, filters.nights);
     }
   }
